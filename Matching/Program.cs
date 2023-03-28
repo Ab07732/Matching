bd8239468ea1cf944e32a4d3fa09d41ea7405308
Austin Branham <austinhb1993@gmail.com>
2023-03-28 13:50:31 -0600

    Add project files.

---
+++ Matching/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var list = new List<string> { "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "B", "B", "B",
+            "B", "B", "B", "B", "B", "R", "R"};
+            int index = rnd.Next(list.Count);
+            list.OrderBy(x => rnd.Next()).Take(2);
+            //if (object1 == list.Count)
+            //{
+
+            //}
+            Console.WriteLine(list[index]);
+        }
+    }
+}
